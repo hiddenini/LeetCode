5e015fb157283c68e584e16ce55d3e17f19bf3ce
bonatgy <1300151235@qq.com>
2021-03-15 11:17:14 +0800

    回文链表(快慢指针 和翻转链表)

--- src/main/java/linkedList/ListNode.java
+++ src/main/java/linkedList/ListNode.java
@@ -8,7 +8,13 @@ package linkedList;
 public class ListNode {
     int val;
     ListNode next;
+
     ListNode(int x) {
         val = x;
+    }
+
+    @Override
+    public String toString() {
+        return val + " -> " + next;
     }
 }
--- src/main/java/linkedList/_234_回文链表.java
+++ src/main/java/linkedList/_234_回文链表.java
@@ -7,6 +7,74 @@ public class _234_回文链表 {
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
         if (head.next.next == null) return head.val == head.next.val;
-        return false;
+        //找到中间节点
+        ListNode mid = middleNode(head);
+        //翻转右半部分(中间节点的右边部分)
+        ListNode rHead = reverseList(mid.next);
+        ListNode lHead = head;
+        ListNode rOldHead = rHead;
+        boolean result = true;
+        while (rHead != null) {
+            if (lHead.val != rHead.val) {
+                result = false;
+                break;
+            }
+            rHead = rHead.next;
+            lHead = lHead.next;
+        }
+        //恢复右半部分,前面打乱了原有链表的结构,非必要,如果要求不能打乱结构的话
+        reverseList(rOldHead);
+        return result;
+    }
+
+    /**
+     * 使用快慢指针找到中间节点
+     * 翻转链表
+     *
+     * @param head
+     * @return
+     */
+    private ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
+    }
+
+    /**
+     * 使用快慢指针找到中间节点
+     *
+     * @param head
+     * @return
+     */
+    private ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        /**
+         * fast.next != null  节点是奇数个
+         *
+         * fast.next.next != null 节点是偶数个
+         */
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next= new ListNode(1);
+        System.out.println(head);
+        _234_回文链表 obj = new _234_回文链表();
+        obj.isPalindrome(head);
+        System.out.println(head);
     }
 }
